0bf42b241448154bc6e19ac131837c1b182c20bc
Marco Hutter <javagl@javagl.de>
2017-05-29 16:31:19 +0200

    Removed ModuleCreators that cannot be persisted

--- flow-module-creation/src/main/java/de/javagl/flow/module/creation/ModuleCreators.java
+++ flow-module-creation/src/main/java/de/javagl/flow/module/creation/ModuleCreators.java
@@ -28,15 +28,8 @@ package de.javagl.flow.module.creation;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import java.util.Objects;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 import de.javagl.flow.module.Module;
-import de.javagl.flow.module.ModuleInfo;
-import de.javagl.flow.module.ModuleInfos;
-import de.javagl.flow.module.Modules;
 import de.javagl.reflection.Constructors;
 import de.javagl.reflection.Methods;
 import de.javagl.reflection.ReflectionException;
@@ -46,97 +39,6 @@ import de.javagl.reflection.ReflectionException;
  */
 public class ModuleCreators
 {
-    /**
-     * Create a {@link ModuleCreator} that will use the given supplier to
-     * create the {@link Module} instances
-     * 
-     * @param moduleInfo The {@link ModuleInfo}
-     * @param moduleSupplier The supplier for the {@link Module} instances
-     * @return The {@link ModuleCreator}
-     */
-    public static ModuleCreator createForModuleSupplier(
-        ModuleInfo moduleInfo, Supplier<? extends Module> moduleSupplier)
-    {
-        Objects.requireNonNull(moduleSupplier, 
-            "The moduleSupplier may not be null");
-        return new AbstractModuleCreator(moduleInfo)
-        {
-            @Override
-            public Module createModule()
-            {
-                return moduleSupplier.get();
-            }
-        };
-    }
-    
-    /**
-     * Create a {@link ModuleCreator} that creates {@link Module} instances
-     * based on the given supplier
-     * 
-     * @param <T> The supplied type
-     * 
-     * @param name The name 
-     * @param description The description
-     * @param supplier The supplier
-     * @param type The supplied type
-     * @return The {@link ModuleCreator}
-     */
-    public static <T> ModuleCreator createForSupplier(
-        String name, String description, Supplier<T> supplier, Class<?> type)
-    {
-        ModuleInfo moduleInfo = 
-            ModuleInfos.createForSupplier(name, description, type);
-        return createForModuleSupplier(moduleInfo, 
-            () -> Modules.createForSupplier(moduleInfo, supplier));
-    }
-
-    /**
-     * Create a {@link ModuleCreator} that creates {@link Module} instances
-     * based on the given consumer
-     * 
-     * @param <T> The consumed type 
-     * 
-     * @param name The name 
-     * @param description The description
-     * @param consumer The consumer
-     * @param type The consumed type
-     * @return The {@link ModuleCreator}
-     */
-    public static <T> ModuleCreator createForConsumer(
-        String name, String description, Consumer<T> consumer, Class<?> type)
-    {
-        ModuleInfo moduleInfo = 
-            ModuleInfos.createForConsumer(name, description, type);
-        return createForModuleSupplier(moduleInfo, 
-            () -> Modules.createForConsumer(moduleInfo, consumer));
-    }
-    
-    /**
-     * Create a {@link ModuleCreator} that creates {@link Module} instances
-     * based on the given function
-     * 
-     * @param <S> The argument type of the function
-     * @param <T> The return type of the function
-     * 
-     * @param name The name 
-     * @param description The description
-     * @param function The function
-     * @param argumentType The argument type of the function
-     * @param resultType The result type of the function
-     * @return The {@link ModuleCreator}
-     */
-    public static <S, T> ModuleCreator createForFunction(
-        String name, String description, Function<S, T> function,
-        Class<?> argumentType, Class<?> resultType)
-    {
-        ModuleInfo moduleInfo = 
-            ModuleInfos.createForFunction(
-                name, description, argumentType, resultType);
-        return createForModuleSupplier(moduleInfo, 
-            () -> Modules.createForFunction(moduleInfo, function));
-    }
-    
-    
     /**
      * Create a {@link ModuleCreator} that will create {@link Module} instances
      * that call the given method
